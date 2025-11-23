5ffbb4cdc7eecad69a31dab4d1e1e18245224563
afreedCt <ctafreed278@gmail.com>
2025-11-23 09:25:43 +0530

    removed unwanted code

--- server/index.js
+++ server/index.js
@@ -28,42 +28,8 @@ const io = new Server(server, {
 
 setupSocket(io)
 
-// io.on("connection", (socket) => {
+const PORT=process.env.PORT
 
-//   socket.on("user_connected", (username) => {
-//     userSocketMap[username] = socket.id;
-//     socket.username = username;
-//     io.emit("online_users", Object.keys(userSocketMap));
-//     console.log("User connected:", username); 
-//     // console.log("User socket map:", userSocketMap);
-//   });
-
-//   socket.on("join_private", ({ user1, user2 }) => {
-//     const room = [user1, user2].sort().join("_");
-//     socket.join(room);
-//     // console.log("user socket map:", userSocketMap);
-//     console.log(`${socket.username} joined ${room}`);
-//   });
-
-//   socket.on("send_message", (data) => {
-//     console.log("data",data)
-//     const room = data.room;
-//     messages.push(data);
-//     console.log("mesagges : ",messages)
-//     io.to(room).emit("receive_message", data);
-//     console.log("Message sent to room:", room, data);
-//   });
-
-//   socket.on("disconnect", () => {
-//     if (socket.username) {
-//       delete userSocketMap[socket.username];
-//       io.emit("online_users", Object.keys(userSocketMap));
-//     }
-//     console.log("User disconnected:", socket.id,userSocketMap);
-//   });
-// });
-
-
-server.listen(3000, () => {
-  console.log("Server running on 5000");
+server.listen(PORT, () => {
+  console.log("Server running on "+PORT);
 });
